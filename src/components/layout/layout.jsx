ebc88262d0985356e07ad1fb1d9444df7b1f1a6d
thathsaraDinu <wthathsaradinuwan@gmail.com>
2025-03-16 18:11:26 +0530

    refactor layout component for improved structure and styling

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,16 +1,16 @@
-import { Outlet } from "react-router-dom";
-import { Header } from "./header";
-import { Sidebar } from "./sidebar";
+import { Outlet } from 'react-router-dom';
+import { Header } from './header';
+import { Sidebar } from './sidebar';
 
 export function Layout() {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex h-screen bg-gray-50">
       <div className="w-64 flex-shrink-0 shadow-lg">
         <Sidebar />
       </div>
-      <div className="grow overflow-hidden w-full">
-        {/* <Header /> */}
-        <main className="grow overflow-y-auto bg-gray-50 p-6">
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
           <div className="mx-auto max-w-7xl">
             <Outlet />
           </div>
